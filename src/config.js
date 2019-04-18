bec954e336574fab1eddc34f7de019d8fe911b7e
Alex Varghese <alex.varghese250@gmail.com>
2019-04-18 12:18:47 -0400

    added api on endpoint

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: 'https://glacial-atoll-57651.herokuapp.com/',
+  API_ENDPOINT: 'https://glacial-atoll-57651.herokuapp.com/api',
   TOKEN_KEY: 'thingful-client-auth-token',
 };
